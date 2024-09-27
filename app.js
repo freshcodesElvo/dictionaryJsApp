1b543557a1c07abcb80b24d69cd03bcc1968880a
Elvis Mudamba <elvismudamba@gail.com>
2024-09-27 13:45:42 +0300

    final app

--- app.js
+++ app.js
@@ -5,31 +5,60 @@ const search_btn = document.querySelector(".search-btn");
 let voice;
 search_btn.addEventListener("click", () => {
     let user_input = document.querySelector(".user-input").value;   
-    console.log(user_input);
+    if (user_input == '') {
+        main_body.innerHTML = `<h3>Please enter a word.</h3>` 
 
-    fetch(`${url}${user_input}`).then(response => response.json()).then(data => {
-        console.log(data);
-        main_body.innerHTML = 
-        `
-            <div class="word-itself">
-                <p>${user_input}</p>
-                
-                <ion-icon onclick ="playSound()" class="sound" name="volume-high"></ion-icon>
-                
-            </div>
-            <div class="type-of-word">${data[0].meanings[0].partOfSpeech} ${data[0].phonetic}</div>
-            <div id="wordDefinition">${data[0].meanings[0].definitions[0].definition}</div>
-            <div id="wordSynonyms"></div>
-            <div id="wordAntonyms"></div>
-            <div id="wordExampleSentences">${data[0].meanings[0].definitions[0].example || ""}</div>
+    }
+    else{
+        console.log(user_input);
 
-        
-        `
-        sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
-  
-    })
-})
+        fetch(`${url}${user_input}`).then(response => response.json()).then(data => {
+            console.log(data);
+            main_body.innerHTML = 
+            `
+                <div class="word-itself">
+                    <p class ="main-word">${user_input}</p>
+                    
+                    <ion-icon onclick ="playSound()" class="sound" name="volume-high">
+                    
+                    
+                    </ion-icon>
+                    
+                </div>
+                <div class="type-of-word">${data[0].meanings[0].partOfSpeech} ${data[0].phonetic}</div>
+                <div id="wordDefinition">${data[0].meanings[0].definitions[0].definition}</div>
+                
+                <div id="wordAntonyms"></div>
+                <p id="wordExampleSentences"> ${data[0].meanings[0].definitions[0].example || ""} </p>
+                <div id="wordSynonyms"> <span> Synonyms: </span> ${data[0].meanings[0].synonyms.join(" ,")}</div>
+            `
+          
+            
+            // sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+    
+            sound = new Audio(data[0].phonetics[0].audio);
+            setTimeout(() => {
+                document.querySelector(".user-input").value = ''; // Clear user input after successful fetch.
+            }, 3000); // Wait for 1 second before trying to play the sound.
+            
+      
+        })
+       
+        .catch( error  => {
+            
+            main_body.innerHTML = `<h3>An error occurred while fetching data. Please try again.</h3>` 
+            sound = null; // Reset sound reference to null to prevent further issues.
+        });
+    }
+});
+
 
 function playSound() {
-    sound.play()
+    if (sound) {
+        sound.play().catch(error => {
+            console.error("Error playing sound:", error);
+        });
+    } else {
+        prompt("Sound element not found or not loaded.");
+    }
 }
